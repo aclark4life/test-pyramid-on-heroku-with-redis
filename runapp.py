d1e54cded89911c6bf19594733bdf88fe79f2094
Alex Clark <aclark@aclark.net>
2012-05-03 19:02:07 -0400

    Use a different hello world

--- runapp.py
+++ runapp.py
@@ -1,9 +1,17 @@
-from paste.deploy import loadapp
-from waitress import serve
-import os
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.response import Response
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app = loadapp('config:production.ini', relative_to='.')
-    serve(app, host='0.0.0.0', port=port)
+def hello_world(request):
+   return Response('Hello %(name)s!' % request.matchdict)
+
+
+if __name__ == '__main__':
+	config = Configurator()
+	config.add_route('hello', '/hello/{name}')
+	config.add_view(hello_world, route_name='hello')
+	app = config.make_wsgi_app()
+	port = int(os.environ.get("PORT", 5000))
+	server = make_server('0.0.0.0', port, app)
+	server.serve_forever()
