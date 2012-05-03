1055d8c227f1628e02540defb82d9ec54172aff4
Alex Clark <aclark@aclark.net>
2012-05-03 19:16:13 -0400

    Add redis ; simplify

--- runapp.py
+++ runapp.py
@@ -2,15 +2,22 @@ from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
 import os
+import urlparse
+
+if 'REDISTOGO_URL' in os.environ:
+    urlparse.uses_netloc.append('redis')
+    url = urlparse.urlparse(os.environ['REDISTOGO_URL'])
+    REDIS = redis.Redis(host=url.hostname, port=url.port, db=0, password=url.password)
+	REDIS.set("answer", 42)
 
 
 def hello_world(request):
-   return Response('Hello %(name)s!' % request.matchdict)
+	return Response('The answer is %s' % REDIS.get("answer"))
 
 
 if __name__ == '__main__':
 	config = Configurator()
-	config.add_route('hello', '/hello/{name}')
+	config.add_route('hello', '/')
 	config.add_view(hello_world, route_name='hello')
 	app = config.make_wsgi_app()
 	port = int(os.environ.get("PORT", 5000))
